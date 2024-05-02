8cd5c8028c0d3b15596ace931861a4004be19a1e
sofia-lpz <a01028251@tec.mx>
2024-05-02 13:33:53 -0600

    red html

--- regex.py
+++ regex.py
@@ -6,6 +6,21 @@ pattern = r'\d+' # \d+ significa uno o más dígitos
 string = '123 hola mundo cruel etc etc 456'
 repl = 'aaa' 
 
-result = re.sub(pattern, repl, string, count=0, flags=0)
+a = re.sub(pattern, repl, string, count=0, flags=0)
 
-print(result)
+print(a)
+
+# Funcion que recibe un token y lo sustituye por su representacion en html con color
+
+def sustituye(objeto):
+    return '<font color="red">' + objeto.group(0) + '</font>'
+
+b = re.sub(pattern, sustituye, string)
+
+print(b)
+
+# escribe el resultado en un archivo html
+
+f2 = open('resultadoRojo.html', 'w')
+f2.write(b)
+f2.close()
